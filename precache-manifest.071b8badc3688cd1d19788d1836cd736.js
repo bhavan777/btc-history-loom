977d0b2ded95639df1b1508783924edaccf43a02
bhavan777 <bhavanvitu@gmail.com>
2018-11-28 09:35:31 +0530

    Updates

---
+++ precache-manifest.071b8badc3688cd1d19788d1836cd736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e7c089a8e84a337ae3c",
+    "url": "/btc-history-loom/static/js/runtime~main.5e7c089a.js"
+  },
+  {
+    "revision": "c7c70449df67268a3d32",
+    "url": "/btc-history-loom/static/js/main.c7c70449.chunk.js"
+  },
+  {
+    "revision": "709d9a20d025f0336051",
+    "url": "/btc-history-loom/static/js/1.709d9a20.chunk.js"
+  },
+  {
+    "revision": "c7c70449df67268a3d32",
+    "url": "/btc-history-loom/static/css/main.8d79c5c5.chunk.css"
+  },
+  {
+    "revision": "de48e1c266b174a9253051c22ec48874",
+    "url": "/btc-history-loom/index.html"
+  }
+];
